2c66c2ab5d6fc59c50834ccb7526ac1500d19405
brminnick <brandon.r.minnick@gmail.com>
2016-06-30 10:59:47 -0700

    Updated NuGet Packages, Added //Arrange //Act //Assert Comments

--- UITests/Tests/Tests.cs
+++ UITests/Tests/Tests.cs
@@ -14,69 +14,86 @@ namespace SimpleUITestApp.UITests
 		[Test]
 		public void EnterText()
 		{
+			//Arrange
 			var textInput = "Hello World";
 
+			//Act
 			FirstPage.EnterText(textInput);
 			FirstPage.ClickGo();
 			FirstPage.WaitForNoActivityIndicator();
 
+			//Assert
 			Assert.AreEqual(FirstPage.GetEntryFieldText(), textInput);
 		}
 
 		[Test]
 		public void EnterTextByID()
 		{
+			//Arrange
 			var textInput = "I used IDs to Enter this Text!";
 
+			//Act
 			FirstPage.EnterTextByID(textInput);
 			FirstPage.ClickGoByID();
 			FirstPage.WaitForNoActivityIndicator();
 
+			//Assert
 			Assert.AreEqual(FirstPage.GetEntryFieldTextByID(), textInput);
 		}
 
 		[Test]
 		public void SelectItemOnListView()
 		{
+			//Arrange
 			var listItemNumber = 9;
 			var expectedAlertString = $"You Selected Number {listItemNumber}";
 
+			//Act
 			FirstPage.ClickListViewButton();
 			ListViewPage.TapListItemNumber(listItemNumber);
 
+			//Assert
 			Assert.AreEqual(expectedAlertString, ListViewPage.GetAlertText(listItemNumber));
 		}
 
 		[Test]
 		public void SelectItemOnListViewByID()
 		{
+			//Arrange
 			var listItemNumber = 9;
 			var expectedAlertString = $"You Selected Number {listItemNumber}";
 
+			//Act
 			FirstPage.ClickListViewButtonByID();
 			ListViewPage.TapListItemNumber(listItemNumber);
 
+			//Assert
 			Assert.AreEqual(expectedAlertString, ListViewPage.GetAlertText(listItemNumber));
 		}
 
 		[Test]
 		public void RotateScreenAndEnterTextByID()
 		{
+			//Arrange
 			var entryTextLandcape = "The Screen Orientation Is Landscape";
 			var entryTextPortrait = "The Screen Orientation Is Portrait";
 
+			//Act
 			FirstPage.RotateScreenToLandscape();
 			FirstPage.EnterText(entryTextLandcape);
 			FirstPage.ClickGoByID();
 			FirstPage.WaitForNoActivityIndicator();
 
+			//Assert
 			Assert.AreEqual(FirstPage.GetEntryFieldTextByID(), entryTextLandcape);
 
+			//Act
 			FirstPage.RotateScreenToPortrait();
 			FirstPage.EnterText(entryTextPortrait);
 			FirstPage.ClickGoByID();
 			FirstPage.WaitForNoActivityIndicator();
 
+			//Assert
 			Assert.AreEqual(FirstPage.GetEntryFieldTextByID(), entryTextPortrait);
 		}
 	}
