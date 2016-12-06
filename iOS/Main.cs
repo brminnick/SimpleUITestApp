f605178c60871f0acef3438938a6e5d361d1b047
brminnick <brandon.r.minnick@gmail.com>
2016-12-05 18:53:51 -0800

    Updated Mobile Center NuGet Packages

--- iOS/Main.cs
+++ iOS/Main.cs
@@ -11,8 +11,7 @@ namespace SimpleUITestApp.iOS
 		// This is the main entry point of the application.
 		static void Main(string[] args)
 		{
-			MobileCenter.Configure(AnalyticsConstants.MOBILE_CENTER_iOS_API_KEY);
-			AnalyticsHelpers.Start();
+			AnalyticsHelpers.Start(AnalyticsConstants.MOBILE_CENTER_iOS_API_KEY);
 
 			// if you want to use a different Application Delegate class from "AppDelegate"
 			// you can specify it here.
